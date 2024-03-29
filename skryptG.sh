c3c7a643d75c84e08234b7b6c5b70931564a57e0
Dominika <dominika.pucyk@icloud.com>
2024-03-29 16:01:37 +0100

    Dodalam obsluge owych flag --init, --error, -e oraz zmodyfikowalam --help

--- skryptG.sh
+++ skryptG.sh
@@ -2,11 +2,11 @@
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        --date)
+        --date|-d)
             echo $(date)
             exit 0
             ;;
-        --logs)
+        --logs|-l)
             if [[ "$2" =~ ^[0-9]+$ ]]; then
                 count=$2
                 shift
@@ -18,18 +18,40 @@ while [[ $# -gt 0 ]]; do
             done
             exit 0
             ;;
-        --help)
+        --help|-h)
             echo "Usage: skryptG.sh [OPTIONS]"
             echo "Options:"
-            echo "  --date      Display today's date"
-            echo "  --logs [N]  Create N log files (default: 100)"
-            echo "  --help      Display this help message"
+            echo "  --date, -d      Display today's date"
+            echo "  --logs [N], -l  Create N log files (default: 100)"
+            echo "  --help, -h      Display this help message"
+	    echo "  --init          Clone the repository and add it to PATH"
+            echo "  --error [N], -e Create N error files (default: 100)"
+            exit 0
+            ;;
+        --init)
+            git clone https://github.com/dominika7777/Lab4.git
+            export PATH="$PATH:$(pwd)/Lab4"
+            echo "Repository cloned successfully and added to PATH."
+            exit 0
+            ;;
+        --error|-e)
+            if [[ "$2" =~ ^[0-9]+$ ]]; then
+                count=$2
+                shift
+            else
+                count=100
+            fi
+            mkdir -p errorx
+            for (( i=1; i<=$count; i++ )); do
+                echo "Error $i, Created by skrypt.sh, $(date)" > errorx/error$i.txt
+            done
             exit 0
             ;;
         *)
             echo "Unknown option: $1. Use --help for usage."
             exit 1
             ;;
+
     esac
     shift
 done
